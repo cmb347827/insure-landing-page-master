0dc432d5deacac5ffbcf38c6543efa3d12783ea2
cmb347827 <v38d422ll621dem@gmail.com>
2023-10-08 01:27:15 -0400

    Added source and resource files

---
+++ insure-landing-page-master.js
@@ -0,0 +1,32 @@
+'use strict'; 
+
+const colors ={
+	'Almost White': 'hsl(0, 0%, 98%)',
+	'Lighter Gray': 'hsl(11, 2%, 95%)',
+	'Overlay': 'hsla(8, 24%, 2%, 0.5)',
+};
+
+$(window).resize(function(){
+	location.reload();
+});
+const onClick = (selector, handler) => {
+  document.querySelector(selector).addEventListener('click', handler);
+};
+
+
+$(window).on('load',function(){
+		
+	$("#navbarCollapse").on("show.bs.collapse", function(){
+        //menu is collapsed, show close icon.
+		//$('#open').addClass('hidden');
+		//$('#close').addClass('show').css({'position':'relative','bottom':'5px'});
+		$('#open').toggleClass('hidden');
+		$('#close').toggleClass('show');
+    });
+	$("#navbarCollapse").on("hide.bs.collapse", function(){
+        $('#close').toggleClass('show');
+		$('#open').toggleClass('hidden');
+		
+    });
+	  
+});
